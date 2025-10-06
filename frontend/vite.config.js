ae8370cc760d40b1f6fddde9cbf6b38357dcdccd
MillaMirjami <milla.sukki@gmail.com>
2025-10-06 09:53:42 +0300

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+  proxy: {
+    // Tämä ohjaa kaikki pyynnöt /api/... backendille
+    '/api': {
+      target: 'http://localhost:3001', // Backendin URL
+      changeOrigin: true,              // Vaihda alkuperä, jos palvelin vaatii sen
+      secure: false,                   // Ei käytetä HTTPS:ää (kehityksessä)
+    },
+  },
+},
 })
