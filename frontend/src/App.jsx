14d058c6209a9deee8b08ea5953110f01289fd85
l-efa <lefa.sahi@gmail.com>
2025-10-06 12:45:53 +0300

    app.jsx protectedRoute fix ja deleteFavourite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import LogIn from "./pages/Login";
@@ -19,12 +19,64 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import CompanyPage from "./pages/private/CompanyPage";
 import CompanyActivePosts from "./pages/private/CompanyActivePosts.jsx";
 import CompanyPostRequests from "./pages/private/CompanyPostRequests.jsx";
-import CompanyAllPosts from "./pages/private/CompanyAllPosts.jsx"
-import CompanyTrash from "./pages/private/CompanyTrash.jsx"
-import CompanySettings from "./pages/private/CompanySettings.jsx"
-import PublicHomePage from "./pages/public/PublicHomePage.jsx"
+import CompanyAllPosts from "./pages/private/CompanyAllPosts.jsx";
+import CompanyTrash from "./pages/private/CompanyTrash.jsx";
+import CompanySettings from "./pages/private/CompanySettings.jsx";
+import PublicHomePage from "./pages/public/PublicHomePage.jsx";
 
 export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/homepage" element={<PublicHomePage />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/:title/:company" element={<HobbyPage />} />
+        <Route path="/profile" element={<LandingPage />} />
+
+        {/* =================== USER PROTECTED ROUTES =================== */}
+        <Route element={<ProtectedRoute allowedRanks={["user"]} />}>
+          <Route path="/user/homepage" element={<HomePage />} />
+          <Route path="/user/map" element={<MapPage />} />
+          <Route path="/user/categories" element={<CategoryPage />} />
+          <Route path="/user/:title/:company" element={<HobbyPage />} />
+          <Route path="/user/profile" element={<ProfilePage />} />
+        </Route>
+
+        {/* =================== ADMIN PROTECTED ROUTES =================== */}
+        <Route element={<ProtectedRoute allowedRanks={["admin"]} />}>
+          <Route path="/admin" element={<AdminRoute />} />
+          <Route path="/admin/allposts" element={<AllPosts />} />
+          <Route path="/admin/activeposts" element={<ActivePosts />} />
+          <Route path="/admin/postrequests" element={<PostRequests />} />
+          <Route path="/admin/trash" element={<Trash />} />
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/admin/settings" element={<Settings />} />
+        </Route>
+
+        {/* =================== COMPANY PROTECTED ROUTES =================== */}
+        <Route element={<ProtectedRoute allowedRanks={["company"]} />}>
+          <Route path="/company" element={<CompanyPage />} />
+          <Route path="/company/activeposts" element={<CompanyActivePosts />} />
+          <Route
+            path="/company/postrequests"
+            element={<CompanyPostRequests />}
+          />
+          <Route path="/company/allposts" element={<CompanyAllPosts />} />
+          <Route path="/company/trash" element={<CompanyTrash />} />
+          <Route path="/company/settings" element={<CompanySettings />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+/*
+  {
   return (
     <Router>
       <Routes>
@@ -39,7 +91,6 @@ export default function App() {
 
 
 
-{/*
          <Route path="/user/homepage" element={
           <ProtectedRoute allowedRanks={["user"]}>
             <HomePage />
@@ -121,9 +172,9 @@ export default function App() {
 
 
 
- */}
-
-      </Routes>
-    </Router>
-  );
-}
+          
+          </Routes>
+          </Router>
+        );
+      }
+      */
