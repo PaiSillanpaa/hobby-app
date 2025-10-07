38c1603797bdf46ba856d9a17530188b3908e8cf
l-efa <lefa.sahi@gmail.com>
2025-10-08 02:06:14 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://hobbly-app.onrender.com",
+    origin: "https://hobbly-app-front.onrender.com",
     credentials: true, // allow cookies
   })
 );
