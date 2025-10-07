df52d87c53b07b753f75605fc6694117d6cdd0fd
l-efa <lefa.sahi@gmail.com>
2025-10-08 02:37:12 +0300

    fetch fix

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import MobileAdminRedirectModal from "../components/MobileAdminRedirectModal";
 
-const baseurl = "https://";
+const baseurl = "https://hobbly-app.onrender.com/";
 
 export default function LogIn() {
   const [username, setUsername] = useState("");
@@ -36,15 +36,12 @@ export default function LogIn() {
     const isMobile = window.innerWidth < 768;
 
     try {
-      const res = await fetch(
-        "https://hobbly-app.onrender.com/api/user/login",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          credentials: "include", // <--- important for cookies
-          body: JSON.stringify({ username, password }),
-        }
-      );
+      const res = await fetch(`${baseurl}api/user/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", // <--- important for cookies
+        body: JSON.stringify({ username, password }),
+      });
 
       if (!res.ok) {
         const errData = await res.json();
